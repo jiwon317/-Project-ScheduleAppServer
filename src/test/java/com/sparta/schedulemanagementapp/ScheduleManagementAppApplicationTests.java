9f0bb744405d8930e7e80849a4207c4569d3301d
jiwon317 <chlghqlt12@naver.com>
2024-01-31 11:32:53 +0900

    test

---
+++ src/test/java/com/sparta/schedulemanagementapp/ScheduleManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
